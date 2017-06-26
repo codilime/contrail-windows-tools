4185055d2782def74317cf53907be92c32e9e4fa
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-06-26 10:27:03 +0200

    Suggested changes & changes from other PR

--- CIScripts/BuildDockerDriver.ps1
+++ CIScripts/BuildDockerDriver.ps1
@@ -10,8 +10,8 @@ $ErrorActionPreference = "Stop"
 
 $Env:GOPATH=pwd
 
-mkdir bin/
-cd bin
+New-Item -ItemType Directory ./bin
+Set-Location bin
 
 Write-Host "Installing test runner"
 go get -u -v github.com/onsi/ginkgo/ginkgo
@@ -28,23 +28,23 @@ Write-Host "Precompiling tests"
 .\ginkgo.exe build $srcPath/hns
 .\ginkgo.exe build $srcPath/hnsManager
 
-mv $srcPath/driver/driver.test ./
-mv $srcPath/controller/controller.test ./
-mv $srcPath/hns/hns.test ./
-mv $srcPath/hnsManager/hnsManager.test ./
+Move-Item $srcPath/driver/driver.test ./
+Move-Item $srcPath/controller/controller.test ./
+Move-Item $srcPath/hns/hns.test ./
+Move-Item $srcPath/hnsManager/hnsManager.test ./
 
 Write-Host "Copying Agent API python script"
-cp $srcPath/scripts/agent_api.py ./
+Copy-Item $srcPath/scripts/agent_api.py ./
 
 Write-Host "Intalling MSI builder"
 go get -u -v github.com/mh-cbon/go-msi
 
 Write-Host "Building MSI"
 Push-Location $srcPath
-Invoke-Expression "$Env:GOPATH/bin/go-msi make --msi installer.msi --arch x64 --version 0.1 --src template --out $pwd/gomsi"
+& "$Env:GOPATH/bin/go-msi" make --msi installer.msi --arch x64 --version 0.1 --src template --out $pwd/gomsi
 Pop-Location
 
-mv $srcPath/installer.msi ./
+Move-Item $srcPath/installer.msi ./
 
 $cerp = Get-Content $Env:CERT_PASSWORD_FILE_PATH
 & $Env:SIGNTOOL_PATH sign /f $Env:CERT_PATH /p $cerp installer.msi
