79113b0df0cdd9de70e064112437b586269a2d7d
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-07-07 14:10:57 +0200

    Order of tests

--- CIScripts/Tests/Tests.ps1
+++ CIScripts/Tests/Tests.ps1
@@ -1,8 +1,8 @@
-. $PSScriptRoot\DockerDriverTest.ps1
 . $PSScriptRoot\ExtensionLongLeakTest.ps1
-. $PSScriptRoot\ICMPOverMPLSOverGRETest.ps1
 . $PSScriptRoot\MultiEnableDisableExtensionTest.ps1
-. $PSScriptRoot\SNATTest.ps1
-. $PSScriptRoot\TCPCommunicationTest.ps1
-. $PSScriptRoot\TCPOverMPLSOverGRETest.ps1
+. $PSScriptRoot\DockerDriverTest.ps1
 . $PSScriptRoot\VTestScenariosTest.ps1
+. $PSScriptRoot\TCPCommunicationTest.ps1
+. $PSScriptRoot\ICMPOverMPLSOverGRETest.ps1
+. $PSScriptRoot\TCPOverMPLSOverGRETest.ps1
+. $PSScriptRoot\SNATTest.ps1
