e303c135fc59edcb52c6a51ac09cd76f5b86290f
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2017-07-17 13:20:52 +0200

    JW-878 added Sconstruct copying

--- CIScripts/BuildExtension.ps1
+++ CIScripts/BuildExtension.ps1
@@ -15,12 +15,15 @@ Invoke-BatchFile "$Env:VS_SETUP_ENV_SCRIPT_PATH"
 Write-Host "Cloning repositories"
 git clone -b $Env:VROUTER_BRANCH $Env:VROUTER_REPO_URL vrouter/
 git clone -b $Env:SANDESH_BRANCH $Env:SANDESH_REPO_URL tools/sandesh/
+git clone -b $Env:TOOLS_BRANCH $Env:TOOLS_REPO_URL tools/build/
 
 Write-Host "Copying third-party dependencies"
-mkdir third_party/
+New-Item -ItemType Directory .\third_party
 Copy-Item -Recurse "$Env:THIRD_PARTY_CACHE_PATH\extension\*" third_party\
 Copy-Item -Recurse "$Env:THIRD_PARTY_CACHE_PATH\common\*" third_party\
 Copy-Item -Recurse third_party\cmocka vrouter\test\
+
+Copy-Item tools\build\SConstruct .\
 
 Write-Host "Building Extension and Utils"
 $cerp = Get-Content $Env:CERT_PASSWORD_FILE_PATH
