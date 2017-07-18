1ffbc57b1bef25b982948607f337d857fec0b76e
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-07-18 11:50:06 +0200

    JW-881: Multi-branch build

--- CIScripts/BuildAgent.ps1
+++ CIScripts/BuildAgent.ps1
@@ -13,12 +13,52 @@ Write-Host "Sourcing VS environment variables"
 Invoke-BatchFile "$Env:VS_SETUP_ENV_SCRIPT_PATH"
 
 Write-Host "Cloning repositories"
-git clone -b $Env:TOOLS_BRANCH $Env:TOOLS_REPO_URL tools/build/
-git clone -b $Env:VROUTER_BRANCH $Env:VROUTER_REPO_URL vrouter/
-git clone -b $Env:WINDOWSSTUBS_BRANCH $Env:WINDOWSSTUBS_REPO_URL windows/
-git clone -b $Env:SANDESH_BRANCH $Env:SANDESH_REPO_URL tools/sandesh/
+class Repo {
+    [string] $Url;
+    [string] $Branch;
+    [string] $Dir;
+
+    Repo ([string] $Url, [string] $Branch, [string] $Dir) {
+        $this.Url = $Url
+        $this.Branch = $Branch
+        $this.Dir = $Dir
+    }
+}
+
+$DefaultBranch = "windows"
+$Repos = @(
+    [Repo]::new($Env:TOOLS_REPO_URL, $Env:TOOLS_BRANCH, "tools/build/"),
+    [Repo]::new($Env:SANDESH_BRANCH, $Env:SANDESH_REPO_URL, "tools/sandesh/"),
+    [Repo]::new($Env:GENERATEDS_BRANCH, $Env:GENERATEDS_REPO_URL, "tools/generateDS/"),
+    [Repo]::new($Env:VROUTER_BRANCH, $Env:VROUTER_REPO_URL, "vrouter/"),
+    [Repo]::new($Env:WINDOWSSTUBS_BRANCH, $Env:WINDOWSSTUBS_REPO_URL, "windows/")
+)
+
 git clone -b $Env:CONTROLLER_BRANCH $Env:CONTROLLER_REPO_URL controller/
-git clone -b $Env:GENERATEDS_BRANCH $Env:GENERATEDS_REPO_URL tools/generateDS/
+if ($LASTEXITCODE -ne 0) {
+    throw "Cloning from " + $Env:CONTROLLER_REPO_URL + " failed"
+}
+
+$Repos.ForEach({
+    # If there is custom branch specified, try to clone this branch only
+    if ($_.Branch -ne $DefaultBranch) {
+        git clone -b $_.Branch $_.Url $_.Dir
+        if ($LASTEXITCODE -ne 0) {
+            throw "Cloning from " + $_.Url + " failed"
+        }
+
+        continue
+    }
+
+    # Try to clone from controller branch, then from default
+    git clone -b $Env:CONTROLLER_BRANCH $_.Url $_.Dir
+    if ($LASTEXITCODE -ne 0) {
+        git clone -b $DefaultBranch $_.Url $_.Dir
+        if ($LASTEXITCODE -ne 0) {
+            throw "Cloning from " + $_.Url + " failed"
+        }
+    }
+})
 
 Write-Host "Copying third-party dependencies"
 New-Item -ItemType Directory ./third_party
