231f2fd7fb18d51f5908cd237b430de2d7217cb5
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-07-18 12:48:22 +0200

    Default parameter fix

--- CIScripts/BuildAgent.ps1
+++ CIScripts/BuildAgent.ps1
@@ -19,7 +19,7 @@ class Repo {
     [string] $DefaultBranch;
     [string] $Dir;
 
-    Repo ([string] $Url, [string] $Branch, [string] $Dir, [string] $DefaultBranch = "windows") {
+    Repo ([string] $Url, [string] $Branch, [string] $Dir, [string] $DefaultBranch) {
         $this.Url = $Url
         $this.Branch = $Branch
         $this.Dir = $Dir
@@ -28,11 +28,11 @@ class Repo {
 }
 
 $Repos = @(
-    [Repo]::new($Env:TOOLS_REPO_URL, $Env:TOOLS_BRANCH, "tools/build/"),
-    [Repo]::new($Env:SANDESH_REPO_URL, $Env:SANDESH_BRANCH, "tools/sandesh/"),
-    [Repo]::new($Env:GENERATEDS_REPO_URL, $Env:GENERATEDS_BRANCH, "tools/generateDS/"),
-    [Repo]::new($Env:VROUTER_REPO_URL, $Env:VROUTER_BRANCH, "vrouter/", "windows"),
-    [Repo]::new($Env:WINDOWSSTUBS_REPO_URL, $Env:WINDOWSSTUBS_BRANCH, "windows/"),
+    [Repo]::new($Env:TOOLS_REPO_URL, $Env:TOOLS_BRANCH, "tools/build/", "windows"),
+    [Repo]::new($Env:SANDESH_REPO_URL, $Env:SANDESH_BRANCH, "tools/sandesh/", "windows"),
+    [Repo]::new($Env:GENERATEDS_REPO_URL, $Env:GENERATEDS_BRANCH, "tools/generateDS/", "windows"),
+    [Repo]::new($Env:VROUTER_REPO_URL, $Env:VROUTER_BRANCH, "vrouter/", "windows", "windows"),
+    [Repo]::new($Env:WINDOWSSTUBS_REPO_URL, $Env:WINDOWSSTUBS_BRANCH, "windows/", "windows"),
     [Repo]::new( $Env:CONTROLLER_REPO_URL, $Env:CONTROLLER_BRANCH, "controller/", "windows3.1")
 )
 
