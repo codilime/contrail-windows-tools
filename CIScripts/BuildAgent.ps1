1b6f0b8c810a1f369dee6580c6e0a04a48caddf9
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-07-18 15:29:13 +0200

    Multiple fixes

--- CIScripts/BuildAgent.ps1
+++ CIScripts/BuildAgent.ps1
@@ -33,20 +33,25 @@ $Repos = @(
     [Repo]::new($Env:GENERATEDS_REPO_URL, $Env:GENERATEDS_BRANCH, "tools/generateDS/", "windows"),
     [Repo]::new($Env:VROUTER_REPO_URL, $Env:VROUTER_BRANCH, "vrouter/", "windows"),
     [Repo]::new($Env:WINDOWSSTUBS_REPO_URL, $Env:WINDOWSSTUBS_BRANCH, "windows/", "windows"),
-    [Repo]::new( $Env:CONTROLLER_REPO_URL, $Env:CONTROLLER_BRANCH, "controller/", "windows3.1")
+    [Repo]::new($Env:CONTROLLER_REPO_URL, $Env:CONTROLLER_BRANCH, "controller/", "windows3.1")
 )
 
-$CustomBranches = $Repos.Where({ $_.Branch -ne $_.DefaultBranch }) | Select-Object -Property Branch
+$CustomBranches = @($Repos.Where({ $_.Branch -ne $_.DefaultBranch }) | Select-Object -ExpandProperty Branch -Unique)
 if ($CustomBranches.Count -gt 1) {
-    throw "Custom branches defined for multiple repos"
+    throw "Different custom branches defined for multiple repos"
 }
 
 $Repos.ForEach({
     # If there is custom branch provided, at first try to use it for all repos. Otherwise, use branch specific for this repo.
-    $CustomMultiBranch = $(if ($CustomBranches.Count -eq 1) { return $CustomBranches[0] } else { return $_.Branch })
+    $CustomMultiBranch = $(if ($CustomBranches.Count -eq 1) { $CustomBranches[0] } else { $_.Branch })
+
+    Write-Host $("Cloning " +  $_.Url + " from branch: " + $CustomMultiBranch)
     git clone -b $CustomMultiBranch $_.Url $_.Dir
+
     if ($LASTEXITCODE -ne 0) {
+        Write-Host $("Cloning " +  $_.Url + "; branch: " + $_.Branch)
         git clone -b $_.Branch $_.Url $_.Dir
+
         if ($LASTEXITCODE -ne 0) {
             throw "Cloning from " + $_.Url + " failed"
         }
