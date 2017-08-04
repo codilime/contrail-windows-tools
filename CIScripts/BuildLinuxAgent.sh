752c19cf969f8252b45444fd90a07914eaa478e0
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-08-04 16:06:36 +0200

    Fix vagrant ssh commands

--- CIScripts/BuildLinuxAgent.sh
+++ CIScripts/BuildLinuxAgent.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/michal-clapinski/vrouter-vagrant
 cd vrouter-vagrant
 vagrant up
-vagrant ssh
-cd contrail-vrouter
-scons -Q vrouter
-exit $?
+vagrant ssh -c 'ls; cd contrail-vrouter; scons -Q vrouter; exit $?'
+retcode=$?
+vagrant halt
+vagrant destroy -f
+exit $retcode
