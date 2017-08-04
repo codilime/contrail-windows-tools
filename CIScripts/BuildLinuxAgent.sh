6f5645e2fbbb6c8c9d89857b52019680aeaa7e7d
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-08-04 16:12:05 +0200

    Change order of cleaning up VM

--- CIScripts/BuildLinuxAgent.sh
+++ CIScripts/BuildLinuxAgent.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/michal-clapinski/vrouter-vagrant
 cd vrouter-vagrant
+vagrant halt
+vagrant destroy -f
 vagrant up
 vagrant ssh -c 'ls; cd contrail-vrouter; scons -Q vrouter; exit $?'
 retcode=$?
-vagrant halt
-vagrant destroy -f
 exit $retcode
