cd637020806a2b1ef805b66c7fd8163121fb069a
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-08-07 12:38:16 +0200

    Use git checkout to setup branches.

--- CIScripts/Linux/BuildLinuxAgent.sh
+++ CIScripts/Linux/BuildLinuxAgent.sh
@@ -6,49 +6,30 @@
 # parameters:
 # TOOLS_BRANCH CONTROLLER_BRANCH VROUTER_BRANCH GENERATEDS_BRANCH SANDESH_BRANCH
 
-# we need to somehow inject branches from upstream jenkins job into repo tool
-# manifest, however repo uses static xml files only...
-# padre forgive me for this hack...
-
-git clone https://github.com/codilime/contrail-vnc -b windows
-
-echo "
-<manifest>
-<remote name=\"github\" fetch=\"..\"/>
-
-<default revision=\"windows\" remote=\"github\"/>
-
-<project name=\"contrail-build\" revision=\"$1\" remote=\"github\" path=\"tools/build\">
-  <copyfile src=\"SConstruct\" dest=\"SConstruct\"/>
-</project>
-<project name=\"contrail-controller\" revision=\"$2\" remote=\"github\" path=\"controller\"/>
-<project name=\"contrail-vrouter\" revision=\"$3\" remote=\"github\" path=\"vrouter\"/>
-<project name=\"contrail-generateDS\" revision=\"$4\" remote=\"github\" path=\"tools/generateds\"/>
-<project name=\"contrail-sandesh\" revision=\"$5\" remote=\"github\" path=\"tools/sandesh\"/>
-
-<project name=\"contrail-third-party\" remote=\"github\" path=\"third_party\"/>
-
-</manifest>
-" > contrail-vnc/default.xml
-
-echo "
-<manifest>
-<remote name="github" fetch=".."/>
-
-<default revision="refs/heads/windows" remote="github"/>
-
-<project name=\"contrail-build\" revision=\"$1\" remote=\"github\" path=\"tools/build\">
-  <copyfile src="SConstruct" dest="SConstruct"/>
-</project>
-<project name=\"contrail-vrouter\" revision=\"$3\" remote=\"github\" path=\"vrouter\"/>
-<project name=\"contrail-sandesh\" revision=\"$5\" remote=\"github\" path=\"tools/sandesh\"/>
-
-</manifest>
-" > contrail-vnc/multibranch_manifest.xml
-
 vagrant halt
 vagrant destroy -f
 vagrant up
-vagrant ssh -c 'ls; cd contrail-vrouter; scons -Q vrouter; exit $?'
+vagrant ssh -c "
+ls
+cd contrail-vrouter
+pushd controller
+  git checkout $2
+popd
+pushd tools
+  pushd build
+    git checkout $1
+  popd
+  pushd generateds
+    git checkout $4
+  popd
+  pushd sandesh
+    git checkout $5
+  popd
+popd
+pushd vrouter
+  git checkout $3
+popd
+scons -Q vrouter
+exit $?"
 retcode=$?
 exit $retcode
