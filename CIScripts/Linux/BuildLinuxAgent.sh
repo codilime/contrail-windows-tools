bf3a5436dd9a70f5acb56825c2ef3cb7f3088281
Michał Kostrzewa <kostrzewamc@gmail.com>
2017-08-09 19:09:36 +0200

    Use git checkout X --

--- CIScripts/Linux/BuildLinuxAgent.sh
+++ CIScripts/Linux/BuildLinuxAgent.sh
@@ -11,21 +11,21 @@ vagrant ssh -c "
 ls
 cd contrail-vrouter
 pushd controller
-  git checkout $2
+  git checkout $2 --
 popd
 pushd tools
   pushd build
-    git checkout $1
+    git checkout $1 --
   popd
   pushd generateds
-    git checkout $4
+    git checkout $4 --
   popd
   pushd sandesh
-    git checkout $5
+    git checkout $5 --
   popd
 popd
 pushd vrouter
-  git checkout $3
+  git checkout $3 --
 popd
 scons vrouter contrail-vrouter-agent"
 retcode=$?
