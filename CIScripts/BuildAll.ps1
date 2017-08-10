c040d132c84096652118a742e23badbb1dcf5270
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2017-08-10 15:35:25 +0200

    JW-943 wip: Added build functions

--- CIScripts/BuildAll.ps1
+++ CIScripts/BuildAll.ps1
@@ -1,31 +1,5 @@
-if ($Env:ENABLE_TRACE -eq $true) {
-    Set-PSDebug -Trace 1
-}
-
-# Refresh Path and PSModulePath
-$Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
-$Env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
-
-# Stop script on error
-$ErrorActionPreference = "Stop"
-
-Write-Host "Sourcing VS environment variables"
-Invoke-BatchFile "$Env:VS_SETUP_ENV_SCRIPT_PATH"
-
-Write-Host "Cloning repositories"
-class Repo {
-    [string] $Url;
-    [string] $Branch;
-    [string] $DefaultBranch;
-    [string] $Dir;
-
-    Repo ([string] $Url, [string] $Branch, [string] $Dir, [string] $DefaultBranch) {
-        $this.Url = $Url
-        $this.Branch = $Branch
-        $this.Dir = $Dir
-        $this.DefaultBranch = $DefaultBranch
-    }
-}
+. $PSScriptRoot\InitializeCIScript.ps1
+. $PSScriptRoot\BuildFunctions.ps1
 
 $Repos = @(
     [Repo]::new($Env:DRIVER_REPO_URL, $Env:DRIVER_BRANCH, "src/github.com/codilime/contrail-windows-docker" ,"master"),
@@ -37,24 +11,10 @@ $Repos = @(
     [Repo]::new($Env:CONTROLLER_REPO_URL, $Env:CONTROLLER_BRANCH, "controller/", "windows3.1")
 )
 
-$CustomBranches = @($Repos.Where({ $_.Branch -ne $_.DefaultBranch }) | Select-Object -ExpandProperty Branch -Unique)
-$Repos.ForEach({
-    # If there is only one unique custom branch provided, at first try to use it for all repos.
-    # Otherwise, use branch specific for this repo.
-    $CustomMultiBranch = $(if ($CustomBranches.Count -eq 1) { $CustomBranches[0] } else { $_.Branch })
+Clone-Repos -Repos $Repos
+Contrail-Common-Actions -ThirdPartyCache $Env:THIRD_PARTY_CACHE_PATH -VSSetupEnvScriptPath $Env:VSSetupEnvScriptPath
 
-    Write-Host $("Cloning " +  $_.Url + " from branch: " + $CustomMultiBranch)
-    git clone -b $CustomMultiBranch $_.Url $_.Dir
+Build-DockerDriver -DriverSrcPath $Env:DRIVER_SRC_PATH -SigntoolPath $Env:SIGNTOOL_PATH -CertPath $Env:CERT_PATH, -CertPasswordFilePath $Env:CERT_PASSWORD_FILE_PATH
+Build-Extension -ThirdPartyCache $Env:THIRD_PARTY_CACHE_PATH -SigntoolPath $Env:SIGNTOOL_PATH -CertPath $Env:CERT_PATH, -CertPasswordFilePath $Env:CERT_PASSWORD_FILE_PATH
+Build-Agent -ThirdPartyCache $Env:THIRD_PARTY_CACHE_PATH
 
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host $("Cloning " +  $_.Url + " from branch: " + $_.Branch)
-        git clone -b $_.Branch $_.Url $_.Dir
-
-        if ($LASTEXITCODE -ne 0) {
-            throw "Cloning from " + $_.Url + " failed"
-        }
-    }
-})
-
-& .\Tools\CIScripts\BuildDockerDriver.ps1
-& .\Tools\CIScripts\BuildContrail.ps1
