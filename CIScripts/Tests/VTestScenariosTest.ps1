cfc839cf44d4bea9f69f4afdef4ff09ec0afe540
Michał Cłapiński <michal.clapinski@codilime.com>
2017-08-30 17:28:29 +0200

    JW-1016 added comment about exit code

--- CIScripts/Tests/VTestScenariosTest.ps1
+++ CIScripts/Tests/VTestScenariosTest.ps1
@@ -9,7 +9,10 @@ function Test-VTestScenarios {
     $VMSwitchName = $TestConfiguration.VMSwitchName
     Invoke-Command -Session $Session -ScriptBlock {
         Push-Location C:\Artifacts\
+
+        # we don't need to check the exit code because this script raises an exception on failure
         vtest\all_tests_run.ps1 -VMSwitchName $Using:VMSwitchName -TestsFolder vtest\tests | Write-Host
+
         Pop-Location
     }
 
