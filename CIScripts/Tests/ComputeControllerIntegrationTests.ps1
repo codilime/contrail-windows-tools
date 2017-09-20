2a33bb73b34bca5ae24a44902ab889aa295f0e68
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-09-20 17:56:19 +0200

    Added XMPPDnsData test. Small fixes.

--- CIScripts/Tests/ComputeControllerIntegrationTests.ps1
+++ CIScripts/Tests/ComputeControllerIntegrationTests.ps1
@@ -6,7 +6,6 @@ function Test-ComputeControllerIntegration {
            [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
 
     . $PSScriptRoot\CommonTestCode.ps1
-
     function Get-SSHHostname {
         Param([Parameter(Mandatory = $true)] [SSH.SshSession] $Session)
         $Result = Invoke-SSHCommand -Session $Session.SessionId -Command "hostname"
@@ -16,6 +15,11 @@ function Test-ComputeControllerIntegration {
     function Get-PSHostname {
         Param([Parameter(Mandatory = $true)] [PSSessionT] $Session)
         return Invoke-Command -Session $Session -ScriptBlock { hostname }
+    }
+
+    function Get-PSIPAddress {
+        Param([Parameter(Mandatory = $true)] [PSSessionT] $Session)
+        return Invoke-Command -Session $Session -ScriptBlock { Get-NetIPAddress | Where-Object InterfaceAlias -like "Ethernet0*" | Where-Object AddressFamily -eq IPv4 | Select-Object -ExpandProperty IPAddress }  
     }
 
     function Connect-ToController {
@@ -38,7 +42,18 @@ function Test-ComputeControllerIntegration {
         return $false
     }
 
-    function Assert-ComputeInInDnsAgentList {
+    function Test-ComputeInXMPPDnsData {
+        Param ([Parameter(Mandatory = $true)] [string] $DnsIP,
+               [Parameter(Mandatory = $true)] [string] $ComputeIP)
+        $Out = Invoke-RestMethod "$DnsIP:8092/Snh_ShowAgentXmppDnsData?"
+        $OurNode = $Out.AgentXmppDnsDataResponse.data.list.AgentDnsData.agent | Where-Object "#text" -Like "$ComputeIP*"
+        if($OurNode) { 
+            return $true
+        }
+        return $false
+    }
+
+    function Assert-ComputeInDnsAgentList {
         Param ([Parameter(Mandatory = $true)] [string] $DnsIP,
                [Parameter(Mandatory = $true)] [string] $ComputeHostname)
 
@@ -47,12 +62,29 @@ function Test-ComputeControllerIntegration {
         }
     }
 
-    function Assert-ComputeNotInInDnsAgentList {
+    function Assert-ComputeNotInDnsAgentList {
         Param ([Parameter(Mandatory = $true)] [string] $DnsIP,
                [Parameter(Mandatory = $true)] [string] $ComputeHostname)
 
         if(Test-ComputeInDnsAgentList -DnsIP $DnsIP -ComputeHostname $ComputeHostname) {
             throw "$ComputeHostname found in DnsAgentList! EXPECTED: it's not in DnsAgentList."
+        }
+    }
+    function Assert-ComputeInXMPPDnsData {
+        Param ([Parameter(Mandatory = $true)] [string] $DnsIP,
+               [Parameter(Mandatory = $true)] [string] $ComputeIP)
+
+        if(!(Test-ComputeInXMPPDnsData -DnsIP $DnsIP -ComputeIP $ComputeIP)) {
+            throw "$ComputeIP not found anywhere in XMPPDnsData! EXPECTED: it's in XMPPDnsData."
+        }
+    }
+
+    function Assert-ComputeInXMPPDnsData {
+        Param ([Parameter(Mandatory = $true)] [string] $DnsIP,
+               [Parameter(Mandatory = $true)] [string] $ComputeIP)
+
+        if(Test-ComputeInXMPPDnsData -DnsIP $DnsIP -ComputeIP $ComputeIP) {
+            throw "$ComputeIP found in XMPPDnsData! EXPECTED: it's not in XMPPDnsData."
         }
     }
 
@@ -67,23 +99,44 @@ function Test-ComputeControllerIntegration {
             Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
 
             Write-Host "======> Given our compute node is not in DnsAgentList"
-            Assert-ComputeNotInDnsAgentList -ComputeHostname $ComputeHostname -DnsSession $DnsSession
+            Assert-ComputeNotInDnsAgentList -ComputeHostname $ComputeHostname -DnsIP $DnsIP
 
             Write-Host "======> When all compute services are started"
             Initialize-ComputeServices -Session $Session -TestConfiguration $TestConfiguration
 
             Write-Host "======> Then our compute node appears in DnsAgentList after a while"
             Start-Sleep -Seconds 5
-            Assert-ComputeInDnsAgentList -ComputeHostname $ComputeHostname -DnsSession $DnsSession
+            Assert-ComputeInDnsAgentList -ComputeHostname $ComputeHostname -DnsIP $DnsIP
         })
     }
 
+    function Test-ComputeNodeAppearsInXMPPDnsData {
+        Param ([Parameter(Mandatory = $true)] [PSSessionT] $Session,
+               [Parameter(Mandatory = $true)] [SSH.SshSession] $DnsIP,
+               [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
+
+        $Job.StepQuiet($MyInvocation.MyCommand.Name, {
+
+            Write-Host "===> Running: Test-ComputeNodeAppearsInXMPPDnsData"
+            $ComputeIP = Get-PSIPAddress -Session $Session
+            Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
+
+            Write-Host "======> Given our compute node is not in XMPPDnsData"
+            Assert-ComputeNotInXMPPDnsData -ComputeIP $ComputeIP -DnsIP $DnsIP
+
+            Write-Host "======> When all compute services are started"
+            Initialize-ComputeServices -Session $Session -TestConfiguration $TestConfiguration
+
+            Write-Host "======> Then our compute node appears in XMPPDnsData after a while"
+            Start-Sleep -Seconds 5
+            Assert-ComputeInXMPPDnsData -ComputeIP $ComputeIP -DnsIP $DnsIP
+        })
+    }
 
     $Job.StepQuiet($MyInvocation.MyCommand.Name, {
         Test-ComputeNodeAppearsInDnsAgentList -DnsIP $TestConfiguration.DockerDriverConfiguration.ControllerIP -Session $ComputeSession -TestConfiguration $TestConfiguration
-
+        Test-ComputeNodeAppearsInDnsAgentList -DnsIP $TestConfiguration.DockerDriverConfiguration.ControllerIP -Session $ComputeSession -TestConfiguration $TestConfiguration
         # Test cleanup
         Clear-TestConfiguration -Session $ComputeSession -TestConfiguration $TestConfiguration
     })
-
 }
