79bbdb1736cec2d0012748552cca82c33cfe9ca6
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2017-10-03 16:48:58 +0200

    Tests implementation

--- CIScripts/Tests/Pkt0PipeImplementationTests.ps1
+++ CIScripts/Tests/Pkt0PipeImplementationTests.ps1
@@ -2,6 +2,8 @@ function Test-Pkt0PipeImplementation {
     Param ([Parameter(Mandatory = $true)] [System.Management.Automation.Runspaces.PSSession] $Session,
            [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
 
+    . $PSScriptRoot\CommonTestCode.ps1
+
     function Assert-ExtensionIsRunning {
         Param ([Parameter(Mandatory = $true)] [System.Management.Automation.Runspaces.PSSession] $Session,
                [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
@@ -32,6 +34,8 @@ function Test-Pkt0PipeImplementation {
         Assert-ExtensionIsNotRunning -Session $Session -TestConfiguration $TestConfiguration
 
         Write-Host "======> Then Agent should crash when started"
+        Enable-AgentService -Session $Session
+        Assert-IsAgentServiceDisabled -Session $Session
 
         Write-Host "======> Cleanup"
         Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
@@ -51,8 +55,10 @@ function Test-Pkt0PipeImplementation {
         Assert-ExtensionIsRunning -Session $Session -TestConfiguration $TestConfiguration
 
         Write-Host "======> When Agent is started"
+        Enable-AgentService -Session $Session
 
         Write-Host "======> Then Agent should work"
+        Assert-IsAgentServiceEnabled -Session $Session
 
         Write-Host "======> Cleanup"
         Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
@@ -65,15 +71,20 @@ function Test-Pkt0PipeImplementation {
                [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
 
         Write-Host "===> Running: Test-DisablingExtensionWhenAgentEnabled"
+
+        Write-Host "======> Given Extension and Agent are running"
         Enable-VRouterExtension -Session $Session -AdapterName $TestConfiguration.AdapterName -VMSwitchName $TestConfiguration.VMSwitchName `
             -ForwardingExtensionName $TestConfiguration.ForwardingExtensionName
         Assert-ExtensionIsRunning -Session $Session -TestConfiguration $TestConfiguration
-
-        Write-Host "======> Given Extension and Agent are running"
+        Enable-AgentService -Session $Session
+        Assert-IsAgentServiceEnabled -Session $Session
 
         Write-Host "======> When Extension is disabled"
+        Disable-VRouterExtension -Session $Session -AdapterName $TestConfiguration.AdapterName -VMSwitchName $TestConfiguration.VMSwitchName `
+            -ForwardingExtensionName $TestConfiguration.ForwardingExtensionName
 
         Write-Host "======> Then Agent should crash"
+        Assert-IsAgentServiceDisabled -Session $Session
 
         Write-Host "======> Cleanup"
         Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
@@ -81,31 +92,13 @@ function Test-Pkt0PipeImplementation {
         Write-Host "===> PASSED: Test-DisablingExtensionWhenAgentEnabled"
     }
 
-    function Test-ReenablingExtension {
-        Param ([Parameter(Mandatory = $true)] [System.Management.Automation.Runspaces.PSSession] $Session,
-               [Parameter(Mandatory = $true)] [TestConfiguration] $TestConfiguration)
+    # Initialize Agent Config used for all tests
+    Initialize-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
+    New-AgentConfigFile -Session $Session -TestConfiguration $TestConfiguration
+    Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
 
-        Write-Host "===> Running: Test-ReenablingExtension"
-
-        Write-Host "======> Given Extension and Agent are running"
-        Enable-VRouterExtension -Session $Session -AdapterName $TestConfiguration.AdapterName -VMSwitchName $TestConfiguration.VMSwitchName `
-            -ForwardingExtensionName $TestConfiguration.ForwardingExtensionName
-        Assert-ExtensionIsRunning -Session $Session -TestConfiguration $TestConfiguration
-
-        Write-Host "======> When Extension is disabled"
-
-        Write-Host "======> When Extension is reenabled"
-
-        Write-Host "======> Then Agent should not work"
-
-        Write-Host "======> Cleanup"
-        Clear-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
-
-        Write-Host "===> PASSED: Test-ReenablingExtension"
-    }
-
+    # Run all tests
     Test-StartingAgentWhenExtensionDisabled -Session $Session -TestConfiguration $TestConfiguration
     Test-StartingAgentWhenExtensionEnabled -Session $Session -TestConfiguration $TestConfiguration
     Test-DisablingExtensionWhenAgentEnabled -Session $Session -TestConfiguration $TestConfiguration
-    Test-ReenablingExtension -Session $Session -TestConfiguration $TestConfiguration
 }
