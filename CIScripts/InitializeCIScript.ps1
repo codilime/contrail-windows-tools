53eb72cf92ffe6ffbad41d83ab615750ead5fb86
Wojciech Urbański <wojciech.urbanski@codilime.com>
2017-11-02 16:18:30 +0100

    Do not print out environment variables

--- CIScripts/InitializeCIScript.ps1
+++ CIScripts/InitializeCIScript.ps1
@@ -5,8 +5,6 @@ if ($Env:ENABLE_TRACE -eq $true) {
 
 # Refresh Path
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
-$Env:PSModulePath
-$Env:Path
 
 # Stop script on error
 $ErrorActionPreference = "Stop"
