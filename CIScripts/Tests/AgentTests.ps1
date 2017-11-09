81f925d62e92c7cc57f70a8a9e470f998ad6ba78
Michal Clapinski <michal.clapinski@codilime.com>
2017-11-09 19:34:10 +0100

    JW-1208 added env vars and printing them

--- CIScripts/Tests/AgentTests.ps1
+++ CIScripts/Tests/AgentTests.ps1
@@ -59,10 +59,17 @@ function Test-Agent {
         Initialize-TestConfiguration -Session $Session -TestConfiguration $TestConfiguration
         Invoke-Command -Session $Session -ScriptBlock $DefineTestIfGTestOutputSuggestsThatAllTestsHavePassed
         Invoke-Command -Session $Session -ScriptBlock {
+            $TestConfiguration = $Using:TestConfiguration
+
+            # Those env vars are used by agent tests for determining timeout's threshold
+            # They were copied from Linux unit test job
+            $Env:TASK_UTIL_WAIT_TIME = 10000
+            $Env:TASK_UTIL_RETRY_COUNT = 6000
+
             $ConfigurationFile = "C:\Artifacts\vnswa_cfg.ini"
             $Configuration = Get-Content $ConfigurationFile
-            $VirtualInterfaceName = (Get-NetAdapter -Name "vEthernet (HNSTransparent)").IfName
-            $PhysicalInterfaceName = (Get-NetAdapter -Name "Ethernet1").IfName
+            $VirtualInterfaceName = (Get-NetAdapter -Name $TestConfiguration.VHostName).IfName
+            $PhysicalInterfaceName = (Get-NetAdapter -Name $TestConfiguration.AdapterName).IfName
             $Configuration = $Configuration -replace "name=.*", "name=$VirtualInterfaceName"
             $Configuration = $Configuration -replace "physical_interface=.*", "physical_interface=$PhysicalInterfaceName"
             Set-Content $ConfigurationFile $Configuration
@@ -74,6 +81,7 @@ function Test-Agent {
         $AgentTextExecutables = $AgentTextExecutables | Select -Unique
 
         Foreach ($TestExecutable in $AgentTextExecutables) {
+            Write-Host $Env:TASK_UTIL_WAIT_TIME
             $TestRes = Run-Test -Session $Session -TestExecutable $TestExecutable
             if ($TestRes -ne 0) {
                 $Res = 1
