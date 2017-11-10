886487c58679a655ce2de93dab4d9a25e3ea954a
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-11-10 11:58:56 +0100

    Split Common and VMUtils.

--- CIScripts/Common.ps1
+++
@@ -1,9 +0,0 @@
-# Create PSSessionT alias
-$Accel = [PowerShell].Assembly.GetType("System.Management.Automation.TypeAccelerators")
-$Accel::add("PSSessionT", "System.Management.Automation.Runspaces.PSSession")
-
-function Get-VMCreds {
-    $VMUsername = $Env:VM_USERNAME
-    $VMPassword = $Env:VM_PASSWORD | ConvertTo-SecureString -AsPlainText -Force
-    return New-Object System.Management.Automation.PSCredential($VMUsername, $VMPassword)
-}
