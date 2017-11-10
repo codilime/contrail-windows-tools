6c8420e50f704599d56b9ec18a5fbbd9d56edfc2
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-11-10 16:23:59 +0100

    Fix calling other scripts.

--- CIScripts/SpawnTestbed.ps1
+++ CIScripts/SpawnTestbed.ps1
@@ -3,5 +3,5 @@
 # This will likely be replaced with once whole test env blocks are implemented
 # fully.
 
-$PSScriptRoot\Provision.ps1
-$PSScriptRoot\Deploy.ps1
+. $PSScriptRoot\Provision.ps1
+. $PSScriptRoot\Deploy.ps1
