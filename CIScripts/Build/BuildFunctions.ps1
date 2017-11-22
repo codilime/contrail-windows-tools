6db52dd2dd09eb4a9bc6fcf707c97ffa1788700c
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2017-11-22 18:02:57 +0100

    Actually dont

--- CIScripts/Build/BuildFunctions.ps1
+++ CIScripts/Build/BuildFunctions.ps1
@@ -176,7 +176,6 @@ function Invoke-ExtensionBuild {
     $Job.Step("Copying Extension dependencies", {
         Copy-Item -Recurse "$ThirdPartyCache\extension\*" third_party\
         Copy-Item -Recurse third_party\cmocka vrouter\test\
-        Copy-Item -Recurse "$ThirdPartyCache\certs\*" "third_party\certs\"
     })
 
     $BuildMode = $(if ($ReleaseMode) { "production" } else { "debug" })
